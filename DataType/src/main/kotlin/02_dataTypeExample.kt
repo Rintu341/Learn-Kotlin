411ad3b1a4bbe38911d5ac5c0ceb3e8871304ca1
Sujan Mandal <106155058+Rintu341@users.noreply.github.com>
2023-10-25 07:59:18 +0530

    Update comments 02_dataTypeExample.kt

--- DataType/src/main/kotlin/02_dataTypeExample.kt
+++ DataType/src/main/kotlin/02_dataTypeExample.kt
@@ -12,15 +12,15 @@ class User( val name:String, private val userId:Int ,val address:String)
 //initialize with default value "example@gmail.com"
 class Contact (val id:Int ,var email:String ="example@gmail.com")
 
-
-//===>  Kotlin has data classes which are particularly useful for storing data.
-// Data classes have the same functionality as classes,
-// but they come automatically with additional member functions.
-// These member functions allow you to easily print the instance to readable output,
-// compare instances of a class, copy instances, and more.
-// As these functions are automatically available,
-// you don't have to spend time writing the same boilerplate code for each of your classes.
-
+/*
+===>  Kotlin has data classes which are particularly useful for storing data.
+Data classes have the same functionality as classes,
+but they come automatically with additional member functions.
+These member functions allow you to easily print the instance to readable output,
+compare instances of a class, copy instances, and more.
+As these functions are automatically available,
+you don't have to spend time writing the same boilerplate code for each of your classes.
+*/
 //To declare a data class, use the keyword data:
 data class  Employee (val userId : Int , val userName:String)
 
@@ -45,18 +45,21 @@ fun main()
     println(empObj.toString()) //Prints a readable string of the class instance and its properties.
     //OR
     println(empObj)
-    //======================================
+
+   
     println(empObj2.equals(empObj)) //Compares instances of a class.
     // OR
     println("empObj == empObj2 : ${empObj == empObj2}")
     //empObj == empObj2 : false
 
-    //To create an exact copy of a data class instance, call the .copy()
-    // function on the instance.
+    /*
+    To create an exact copy of a data class instance, call the .copy()
+    function on the instance.
 
-    //To create a copy of a data class instance and change some properties,
-    // call the .copy() function on the instance and add replacement values
-    // for properties as function parameters
+    To create a copy of a data class instance and change some properties,
+    call the .copy() function on the instance and add replacement values
+    for properties as function parameters
+    */
     val firstUser = Employee(4,"Saheb")
     val secondUser = Employee(5,"Lakshman")
 
